f3ab7ae0438863bab21fd6bce47371c425a72c9d
Maksim Kovtun <brain1999@mail.ru>
2021-10-20 23:11:21 +0300

    The solution to the "Letter combinations of a phone number" problem has been improved

--- 0017_letter_combinations_of_a_phone_number.py
+++ 0017_letter_combinations_of_a_phone_number.py
@@ -1,5 +1,32 @@
 class Solution:
-    def letterCombinations(self, digits: str):  # -> List[str]:
+    def letterCombinations(self, digits: str):
+        def get_symbols(phonepad, digit):
+            x = 0
+            length = len(phonepad[digit])
+            while x < length:
+                yield phonepad[digit][x]
+                x += 1
+
+        def recurse_digit(phonepad, digs, sub_result, result):
+            if len(digs) == 1:
+                for letter in get_symbols(phonepad, digs[0]):
+                    result.append(sub_result + letter)
+                return
+            for letter in get_symbols(phonepad, digs[0]):
+                recurse_digit(phonepad, digs[1:], sub_result+letter, result)
+        if len(digits) == 0:
+            return []
+        result = list()
+        phonepad = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+                    "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        if len(digits) == 1:
+            return [letter for letter in get_symbols(phonepad, digits[0])]
+            
+        for letter in get_symbols(phonepad, digits[0]):
+            recurse_digit(phonepad, digits[1:], letter, result)
+        return result
+
+    def letterCombinations_direct(self, digits: str):  # -> List[str]:
         '''17. Letter Combinations of a Phone Number
         Medium
 
@@ -55,8 +82,8 @@ class Solution:
 
 
 solution = Solution()
-assert solution.letterCombinations(
-    "23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+# assert solution.letterCombinations(
+#     "23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 assert solution.letterCombinations("") == []
 assert solution.letterCombinations("2") == ["a", "b", "c"]
 assert solution.letterCombinations("7") == ["p", "q", "r", "s"]
