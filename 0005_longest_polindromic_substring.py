d1eb403e8dee19e0e703bc32ed4ddff8bc1fc469
Maksim Kovtun <brain1999@mail.ru>
2022-09-29 22:15:41 -0400

    First version

---
+++ 0005_longest_polindromic_substring.py
@@ -0,0 +1,53 @@
+'''
+Given a string s, return the longest palindromic substring in s.
+
+A string is called a palindrome string if the reverse of that string is 
+the same as the original string.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+'''
+
+
+class Solution:
+
+    def is_pol(self, substr: str) -> bool:
+        return True if substr == substr[::-1] else False
+
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+        result = ""
+        for left in range(len(s)-1):
+            position = old_position = left
+            while position < len(s) or position != -1:
+                position = s[old_position + 1:].find(s[left])
+                if position == -1:
+                    break
+                old_position = position + old_position + 1
+                sl = s[left:] \
+                    if old_position == len(s) \
+                    else s[left:old_position + 1]
+
+                if self.is_pol(sl) and len(sl) > len(result):
+                    result = sl
+        return s[0] if result == "" else result
+
+
+sol = Solution()
+assert sol.longestPalindrome("cbbd") == "bb"
+assert sol.longestPalindrome("babad") == "bab"
+assert sol.longestPalindrome("bababad") == "babab"
+assert sol.longestPalindrome("babababd") == "bababab"
+assert sol.longestPalindrome("abcdbab") == "bab"
+assert sol.longestPalindrome("a") == "a"
+assert sol.longestPalindrome("ab") == "a"
+s = "tktneonoubkxgfhybavrfnetlxgtkelsoeeuznssntcleenqgiboexflfvlfiapqjbcwyenmfnmxcbcjscucqhwuqfzvvkdxrtxzhjdvsjawcwffoglhkxvyxaninlswyjcfvdwfkqheidwprtjaaqzqgloctafkuasubqqeacdpmtfzokccmnslnklxyvfitbfbdjrlzhkhnturfkimghcnngvdhbehewzzyfsbsactkrfabkhaavryubckkrqbqcbenqpeykyawzkctswaczbjtzeyteftsjklrtchxggsslscypkuilhbitsjwzsvwmqahxkmghigdtuqehjkqswchkrolcloxnkqocyjeorkwjmbevwijmqfhtmolspqcqshafjuxcheckguzxvapfivznkzdkzwnvlquzrbkhvmpdclrettjrxinbbvlwtsyepestvwjfiekjaqphfrhiifrplokslzaxmlwafrrfawlmxazlskolprfiihrfhpqajkeifjwvtsepeystwlvbbnixrjtterlcdpmvhkbrzuqlvnwzkdzknzvifpavxzugkcehcxujfahsqcqpslomthfqmjiwvebmjwkroejycoqknxolclorkhcwsqkjhequtdgihgmkxhaqmwvszwjstibhliukpycslssggxhctrlkjstfetyeztjbzcawstckzwaykyepqnebcqbqrkkcbuyrvaahkbafrktcasbsfyzzwehebhdvgnnchgmikfrutnhkhzlrjdbfbtifvyxlknlsnmcckozftmpdcaeqqbusaukfatcolgqzqaajtrpwdiehqkfwdvfcjywslninaxyvxkhlgoffwcwajsvdjhzxtrxdkvvzfquwhqcucsjcbcxmnfmneywcbjqpaiflvflfxeobigqneelctnssnzueeoslektgxltenfrvabyhfgxkbuonoentkt"
+# print(sol.longestPalindrome(s))
+assert sol.longestPalindrome(s) == s
