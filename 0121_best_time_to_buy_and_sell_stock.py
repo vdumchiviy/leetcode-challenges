fb90eac88e95e3ed976b8d9129604c25ca171245
vdumchiviy <brain1999@mail.ru>
2023-02-01 08:54:22 -0500

    0121. Best Time to Buy and Sell Stock has been solved

--- 0121_best_time_to_buy_and_sell_stock.py
+++ 0121_best_time_to_buy_and_sell_stock.py
@@ -31,22 +31,30 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 0:
-            return 0
         result = 0
-        previous = prices[-1]
-        pos = len(prices) - 2
-        while pos >= 0:
-            if previous > 
+        min_price = max_price = prices[-1]
 
-        for x in range(len(prices)-1, -1, -1):
-            print(x)
+        for price in prices[-1::-1]:
+            if price > max_price:
+                max_price = price
+                min_price = price
+
+            if price < min_price:
+                min_price = price
+                result = max(result, max_price-min_price)
+
+        return result
 
 
 sol = Solution()
 
+assert sol.maxProfit([7, 2, 9, 1, 5, 0]) == 7
+assert sol.maxProfit([7, 2, 9, 1, 5, 15]) == 14
 assert sol.maxProfit([7, 1, 5, 3, 6, 4]) == 5
 assert sol.maxProfit([7, 6, 5, 4, 3, 2, 1]) == 0
 assert sol.maxProfit([7]) == 0
 assert sol.maxProfit([0, 1, 2, 3, 4, 6]) == 6
-assert sol.maxProfit([0, 1, 9, 0, 4, 6]) == 6
+assert sol.maxProfit([0, 1, 9, 0, 4, 6]) == 9
+assert sol.maxProfit([7, 6, 5, 4, 3, 2, 1, 0]) == 0
+assert sol.maxProfit([7, 6]) == 0
+assert sol.maxProfit([6, 7]) == 1
